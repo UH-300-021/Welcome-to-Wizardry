34bcbdda702faae611da54c3f0a3570fa403f1d2
Matthew York <myork@cs.ua.edu>
2015-02-03 15:40:41 -0600

    - Initial Commit

---
+++ WelcomeToWizardy/WelcomeToWizardy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WelcomeToWizardy
+//
+//  Created by UH 300 -21 on 2/3/15.
+//  Copyright (c) 2015 UH 300 -21. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
